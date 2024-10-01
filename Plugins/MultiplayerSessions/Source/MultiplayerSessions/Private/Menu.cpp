d43ac979893c0e7faced15d220e48196b134051a
beggarking1002 <drake1002@naver.com>
2024-10-01 10:09:31 +0900

    Tracking Incoming Players/Path to Lobby
    Tracking Incoming Players/Path to Lobby

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -8,8 +8,9 @@
 #include "OnlineSessionSettings.h"
 #include "OnlineSubsystem.h"
 
-void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch)
+void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch, FString LobbyPath)
 {
+PathToLobby = FString::Printf(TEXT("%s?listen"), *LobbyPath);
 	NumPublicConnections = NumberOfPublicConnections;
 	MatchType = TypeOfMatch;
 	AddToViewport();
@@ -79,19 +80,11 @@ void UMenu::OnCreateSession(bool bWasSuccessful)
 {
 	if(bWasSuccessful)
 	{
-		if(GEngine)
-		{
-			GEngine->AddOnScreenDebugMessage(
-				-1,
-				15.f,
-				FColor::Yellow,
-				FString(TEXT("Host Button Clicked"))
-				);
-		}
+		
 		UWorld* World = GetWorld();
 		if(World)
 		{
-			World->ServerTravel("/Game/ThirdPerson/Maps/Lobby?Listen");
+			World->ServerTravel(PathToLobby);
 		}
 	}
 	else
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -16,7 +16,8 @@ class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
 	GENERATED_BODY()
 public:
 	UFUNCTION(BlueprintCallable)
-	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")));
+	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/ThirdPersonCPP/Maps/Lobby")));
+	
 
 protected:
 	virtual bool Initialize() override;
@@ -57,6 +58,6 @@ private:
 
 	int32 NumPublicConnections{4};
 	FString MatchType{TEXT("FreeForAll")};
-
+	FString PathToLobby{TEXT("")};
 	
 };
